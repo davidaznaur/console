3f5f752194e4e4878682b88ca0cb479d2889a298
David Aznaurov <daznauro@redhat.com>
2026-07-10 09:53:56 -0400

    FCN-247: backend part
    Signed-off-by: David Aznaurov <daznauro@redhat.com>

--- backend/src/routes/rosaWizardApi.ts
+++ backend/src/routes/rosaWizardApi.ts
@@ -1,4 +1,3 @@
-/* Copyright Contributors to the Open Cluster Management project */
 import { Http2ServerRequest, Http2ServerResponse } from 'node:http2'
 import { jsonRequest } from '../lib/json-request'
 import { logger } from '../lib/logger'
@@ -89,6 +88,36 @@ export async function getAwsBillingAccountIds(req: Http2ServerRequest, res: Http
 
         res.setHeader('Content-Type', 'application/json')
         res.end(JSON.stringify(accReq))
+      })
+    } catch (err) {
+      logger.error(err)
+      respondInternalServerError(req, res)
+    }
+  }
+}
+
+export async function getWizardCloudProviders(req: Http2ServerRequest, res: Http2ServerResponse) {
+  const token = await getAuthenticatedToken(req, res)
+  if (token) {
+    try {
+      let data: string = undefined
+      const chucks: string[] = []
+      req.on('data', (chuck: string) => {
+        chucks.push(chuck)
+      })
+
+      req.on('end', async () => {
+        data = chucks.join()
+        const body = JSON.parse(data) as Payload
+        const accessTokenSSO = await getOcmServiceToken(body.service_account_id, body.service_account_secret)
+
+        const cloudProvidersPath = `${API_URL}/api/clusters_mgmt/v1/cloud_providers?size=-1&fetchRegions=true`
+        const request = await jsonRequest(cloudProvidersPath, accessTokenSSO).catch((err: Error) => {
+          logger.error({ msg: 'Failed to fetch regions', error: err.message })
+          return { error: err.message }
+        })
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify(request))
       })
     } catch (err) {
       logger.error(err)
